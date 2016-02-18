e6e6008662b5fd7c3533dd0dfe33f211171ef006
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-02-18 10:35:08 -0500

    remove redundancies in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,6 @@ class ArticlesController < ApplicationController
 		def create
 			@article = Article.new(article_params)
 			@current_admin_name = current_admin.first_name + " " + current_admin.last_name
-			@article.topic_id = params[:article][:topic]
 			@article.author = @current_admin_name
 			if @article.save
 				flash[:success] = "Your article has been saved!"
@@ -46,7 +45,6 @@ class ArticlesController < ApplicationController
 			@article = Article.find(params[:id])
 			@current_admin_name = current_admin.first_name + " " + current_admin.last_name
 			@article.update(article_params)
-			@article.topic_id = params[:article][:topic]
 			@article.update_attributes(author: @current_admin_name)
 			flash[:success] = "'#{@article.title}' has been updated."
 			redirect_to article_path(@article)
