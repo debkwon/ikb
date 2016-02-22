cec194b8dd2358371c5f4814f9cbae81005a8c9b
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-02-22 11:44:00 -0500

    update syntax for postgres compatibility

--- db/schema.rb
+++ db/schema.rb
@@ -19,15 +19,15 @@ ActiveRecord::Schema.define(version: 20160218200813) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.timestamp "created_at",                          null: false
+    t.timestamp "updated_at",                          null: false
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20160218200813) do
     t.string   "title"
     t.text     "content"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.timestamp "created_at", null: false
+    t.timestamp "updated_at", null: false
     t.integer  "topic_id"
     t.boolean  "is_draft"
   end
@@ -48,27 +48,27 @@ ActiveRecord::Schema.define(version: 20160218200813) do
   create_table "bootsy_image_galleries", force: :cascade do |t|
     t.integer  "bootsy_resource_id"
     t.string   "bootsy_resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "bootsy_images", force: :cascade do |t|
     t.string   "image_file"
     t.integer  "image_gallery_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "categories", force: :cascade do |t|
     t.text     "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.timestamp "created_at", null: false
+    t.timestamp "updated_at", null: false
   end
 
   create_table "topics", force: :cascade do |t|
     t.text     "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.timestamp "created_at",  null: false
+    t.timestamp "updated_at",  null: false
     t.integer  "category_id"
   end
 
