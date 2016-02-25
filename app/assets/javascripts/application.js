293ced4f6b079842762b753744950db4ecee444c
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-02-24 21:50:19 -0500

    attempt to fix refresh for AJAX

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require turbolinks
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
 //= require bootsy
+//= require turbolinks
 //= require_tree .
