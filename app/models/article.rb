838ab392be6df7703a52cf65ff3cbb1b965369c4
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-02-26 15:55:55 -0500

    solr reindex and devise flash override

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,15 +1,20 @@
 class Article < ActiveRecord::Base
 	include Bootsy::Container
 	belongs_to :topic
+	belongs_to :category
 	validates :title, presence: true
 	validates :topic_id, presence: true
 	validates	:category_id, presence: true
 
-	def self.search(search)
-		search_list = []
-		sanitized_search = sanitize_sql_array(["to_tsquery('english', ?)", search.gsub(/\s/,"+")])
-		search_list |= where("textsearchable_index_col @@ #{sanitized_search}").order("ts_rank_cd(textsearchable_index_col, #{sanitized_search})desc limit 15")
-		search_list
+	searchable do
+		text :title
+		text :content
+	end
+
+	def search
+		@search = Article.search do
+			keywords(params[:q])
+		end
 	end
 	
 end
